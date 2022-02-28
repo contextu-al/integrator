e5efe3bf0c3b94689b47c452634caacf72767d85
Ganesh Faterpekar <ganeshfaterpekar@gmail.com>
2022-02-28 14:35:47 +1100

    open xcode project

--- Sources/Integrator/Integrationsteps/SwiftIntegration.swift
+++ Sources/Integrator/Integrationsteps/SwiftIntegration.swift
@@ -14,7 +14,7 @@ let searchExpression = ".*didFinishLaunchingWithOptions.*->\\sBool\\s*\\n*\\{"
 let replacemantString = """
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
   
- Pointzi.sharedInstance().registerInstall(forApp: "MyApp", withDebugMode: false)
+ Pointzi.sharedInstance().registerInstall(forApp: "Ganesh_test", withDebugMode: true)
 """
 
 
@@ -22,6 +22,8 @@ func add_Intializer_In_AppDelegate(projectName : String) throws {
     try search_pattern(fileName: "AppDelegate.swift", pattern: searchExpression, replacemantString: replacemantString)
 }
 
-func replace_bases_classes() {}
+func replace_bases_classes() {
+    let _ = shell(command: "sed -i '' 's/UIViewController/StreetHawkBaseViewController/g' ViewController.swift")
+}
 
 func add_shCuid() {}
--- Sources/Integrator/commands/InstallCommand.swift
+++ Sources/Integrator/commands/InstallCommand.swift
@@ -23,28 +23,30 @@ struct InstallSDK: ParsableCommand {
     var projectName: String
     
     func run() throws {
-        
         let finalPath = "\(projectpath)/\(projectName)"
         
         go_to_project_folder(path: projectpath)
         createSampleProject(projectName: projectName)
+        
         go_to_project_folder(path: finalPath)
         copy_xcode_helper_script()
-       
         
         performPodsPreCheck(projectName: projectName)
         try addPodSteps()
+        
         createTemporaryGitRepo()
         try addBridgingHeaders(projectName: projectName, projectPath: projectpath)
+       
         go_to_project_folder(path: finalPath+"/"+projectName)
         try performSwiftIntegraton()
-         
         
+        openXcodeProject(path: finalPath ,name: projectName)
     }
     
     func performPodsPreCheck(projectName: String) {
-        //Pod Prechecking
-        if check_If_file_exists_at(fileName: projectName) &&  !check_If_file_exists_at(fileName: "Podfile") {
+
+        if check_If_file_exists_at(fileName: projectName) &&
+           !check_If_file_exists_at(fileName: "Podfile") {
             create_pod_file()
         }
     }
@@ -56,24 +58,26 @@ struct InstallSDK: ParsableCommand {
     
     func createTemporaryGitRepo() {
         gitInit()
-        
     }
-    
-   
    
     func performSwiftIntegraton() throws {
         do {
             try add_Intializer_In_AppDelegate(projectName : projectName)
+                replace_bases_classes()
             
         } catch {
                 print(error)
         }
-        
     }
 }
-
 
 func addBridgingHeaders(projectName: String ,projectPath: String) throws {
     try create_bridging_header_file(projectName: projectName)
     add_headers_to_xcode_settings(projectPath: projectPath, projectName: projectName)
+}
+
+func openXcodeProject(path: String, name: String) {
+    print("Opening xcode project")
+    go_to_project_folder(path: path)
+    _  = shell(command: "open \(path)/\(name).xcworkspace")
 }
